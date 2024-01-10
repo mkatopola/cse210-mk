c902c9405c2ae1bc702d6e8554d17d975d32410c
mkatopola <moffatkatopola@gmail.com>
2024-01-10 08:29:40 +0200

    Prep1 finished

--- csharp-prep/Prep1/Program.cs
+++ csharp-prep/Prep1/Program.cs
@@ -4,6 +4,14 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep1 World!");
+        // user first name
+        Console.Write("What is your first name? ") ;
+        string first_name = Console.ReadLine() ;
+        // user last name
+        Console.Write("What is your last name? ") ;
+        string last_name = Console.ReadLine() ;
+
+        // user full name
+        Console.WriteLine($"Your name is {last_name}, {first_name} {last_name}.") ;
     }
-}
+} 
