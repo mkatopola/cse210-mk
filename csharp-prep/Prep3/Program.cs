76f7c024503495b5901d98a8430e292bf9f66f07
mkatopola <moffatkatopola@gmail.com>
2024-01-12 15:36:17 +0200

    Prep3 finished

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -1,9 +1,51 @@
 using System;
+using System.Security.Cryptography;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+
+        bool playAgain = true;
+        while (playAgain)
+        {
+            // Console.Write("What is the magic number? ");
+            // string userNumber = Console.ReadLine();
+            Random random = new Random();
+            int number = random.Next(1, 100);
+            int guess = 0;
+            int guessCount = 0;
+
+            while (guess != number)
+            {
+                Console.Write("What is your guess? ");
+                string userGuess = Console.ReadLine();
+                guess = int.Parse(userGuess);
+                guessCount ++;
+
+                if (guess < number)
+                {
+                    Console.WriteLine("Higher");
+                }
+
+                else if (guess > number)
+                {
+                    Console.WriteLine("Lower");
+                }
+
+                else
+                {
+                    Console.WriteLine("You guessed it!");
+                    Console.WriteLine($"It took you {guessCount} guesses.");
+                }
+
+            }
+
+            Console.Write("Do you want to play again? (yes/no) ");
+            string userAnswer = Console.ReadLine();
+            playAgain = userAnswer.Equals("yes", StringComparison.OrdinalIgnoreCase);
+        }    
+            
     }
 }
+
