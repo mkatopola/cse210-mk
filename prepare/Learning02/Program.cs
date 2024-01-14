effaecbc7fdc69226135fd96465c6a386cdf0cd3
mkatopola <moffatkatopola@gmail.com>
2024-01-14 16:24:22 +0200

    Learning02 finished

--- prepare/Learning02/Program.cs
+++ prepare/Learning02/Program.cs
@@ -4,6 +4,34 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning02 World!");
+        Job job1 = new Job();
+        job1._company = "Adventist Health Services";
+        job1._jobTitle = "Intern Laboratory Technician";
+        job1._startYear = 2020;
+        job1._endYear = 2021;
+
+        Job job2 = new Job();
+        job2._company = "Ministry of Health";
+        job2._jobTitle = "Laboratory Technician";
+        job2._startYear = 2021;
+        job2._endYear = 2022;
+
+        Job job3 = new Job();
+        job3._company = "Ministry of Health";
+        job3._jobTitle = "Laboratory Supervisor";
+        job3._startYear = 2023;
+        job3._endYear = 2024;
+
+
+        Resume myResume = new Resume();
+        myResume._nameOfPerson = "Moffat Katopola";
+
+        myResume._jobList.Add(job1);
+        myResume._jobList.Add(job2);
+        myResume._jobList.Add(job3);
+        
+        Console.WriteLine();
+        myResume.DisplayResumeDetails();
+        Console.WriteLine();
     }
 }
---
+++ prepare/Learning02/Resume.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+public class Resume
+{
+    public string _nameOfPerson;
+    public List<Job> _jobList = new List<Job>();
+
+    public void DisplayResumeDetails()
+    {
+        Console.WriteLine($"Name: {_nameOfPerson}");
+        Console.WriteLine("Jobs: ");
+
+        foreach (Job job in _jobList)
+        {
+            job.DisplayJobDetails();
+        }
+    }
+}
