cb5756b864d8bcc519fe29527a3c948821ca55d9
mkatopola <moffatkatopola@gmail.com>
2024-01-27 20:59:33 +0200

    Learning04 Finished

--- prepare/Learning04/Program.cs
+++ prepare/Learning04/Program.cs
@@ -1,9 +1,26 @@
 using System;
 
-class Program
+public class Program
 {
-    static void Main(string[] args)
+    public static void Main()
     {
-        Console.WriteLine("Hello Learning04 World!");
+        Console.WriteLine();
+        // Create a base "Assignment" object
+        Assignment assignment = new Assignment("Samuel Bennett", "Multiplication");
+        Console.WriteLine(assignment.GetSummary());
+        Console.WriteLine();
+
+        // Creating a new MathAssignment object
+        MathAssignment mathAssignment = new MathAssignment("Moffat Katopola", "Calculus", "1.3", "1-10");
+        Console.WriteLine(mathAssignment.GetSummary()); // Calling the GetSummary() method
+        Console.WriteLine(mathAssignment.GetHomeworkList()); // Calling the GetHomeworkList() method
+        Console.WriteLine();
+
+        // Creating new WritingAssignment object
+        WritingAssignment writingAssignment = new WritingAssignment("Mary Waters", "European History", "The Causes of World War II by Mary Waters");
+        Console.WriteLine(writingAssignment.GetSummary()); // Calling the GetSummary() method
+        Console.WriteLine(writingAssignment.GetWritingInformation()); // Calling the GetWritingInformation
+        Console.WriteLine();
+
     }
 }
